d3998905b322891a00ca13a7ecae591bc24c1d91
Jorge Liauw <info@blastcloudmovers.nl>
2022-07-05 15:51:03 +0200

    Add Compute API

--- main.tf
+++ main.tf
@@ -32,9 +32,18 @@ resource "google_project_service" "service_networking" {
   disable_on_destroy         = false
 }
 
+resource "google_project_service" "compute" {
+  #project = var.project_id
+  service                    = "compute.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+
+
 resource "google_project_service" "ids" {
   #project = var.project_id
   service                    = "ids.googleapis.com"
   disable_dependent_services = false
   disable_on_destroy         = false
 }
+
